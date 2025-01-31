ada29ed63e442b65854d0323b0c5f0c66892e799
Muhammad Abdullah <ma3472995@gmail.com>
2025-01-31 10:12:38 +0500

    basic intro to functions, prototype and objects

--- scripts/object.js
+++ scripts/object.js
@@ -5,13 +5,20 @@ let person = { name: 'Abdullah',
     age : 22,
     isStudent: true,
     registeredCourse : {
-        reg1 : "PF",
-        reg2 : "OOP"
+        title : "PF",
+        isPassed : true
+    },
+    displayName : function(){
+        return this.name;
     }
 
 };
 
 console.log(person);
+
+for (key in person){
+    console.log(person[key]);
+}
 
 
 // let person = new Object();
@@ -31,5 +38,43 @@ console.log(person);
 // console.log(person[va]);
 
 
+//person["registeredCourse": { cr1: "OOP", isP : true, cr2 : "DS" , iP : ture, cr3 : "DSA", isPass : false }]
 
+
+// Object.keys(person);
+// Object.values(person);
+
+//Object destructuring
+//let {name , isStudent} = person;
+
+
+// spread with object => ...
+// rest when as parameter => ...
+
+//spread
+//let currStudent = {... person};
+
+
+//rest
+// function add (... val){
+//     let result = 0;
+//     for(c = 0; c<val.length();c++){
+//         result+=val;
+//     }
+// }
+
+
+function createPerson(name, isStudent){
+    this.name = name;
+    this.isStudent = isStudent;
+}
+
+
+let person4 = createPerson("Taqi", true);
+
+
+person.__proto__.semesterStrat = ture;
+person.prototype.greet = function(){
+    return "Hello" + $(this.name);
+}
 
