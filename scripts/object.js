edc5d54cf141ec54344ea7b2dc14abad0a900cb8
Muhammad Abdullah <ma3472995@gmail.com>
2025-01-31 09:24:47 +0500

    Objects Lec
    Created Person object using constructor, Used square bracket, Delete and create fucntions

---
+++ scripts/object.js
@@ -0,0 +1,35 @@
+//Object first lettercapital
+//All caps mean constant
+
+let person = { name: 'Abdullah',
+    age : 22,
+    isStudent: true,
+    registeredCourse : {
+        reg1 : "PF",
+        reg2 : "OOP"
+    }
+
+};
+
+console.log(person);
+
+
+// let person = new Object();
+// person.name = "Abdullah";
+// person.isStudent = true;
+// person.courses = ["CN","DS", "WEB"];
+
+// let person2 = Object.create(null);
+
+// console.log(person.courses);
+
+
+// let va = "name";
+
+// delete person[va];
+
+// console.log(person[va]);
+
+
+
+
