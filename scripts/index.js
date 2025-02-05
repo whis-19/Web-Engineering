6cfb14d5f9ebce60ea83553412d88a9b9d70d8e6
Muhammad Abdullah <ma3472995@gmail.com>
2025-02-06 01:13:04 +0500

    forEach

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,44 @@
-// hello(bye);
+// let numbers = [1, 2, 3, 4, 5];
 
 
-// function hello(bye) {
-//   console.log('Hello');
-//   bye();
+// numbers.forEach(displayNumber);
+// numbers.forEach(evenNumbers);
+// numbers.forEach(oddNumbers);
+
+// function displayNumber(number) {
+//     console.log(number);
 // }
 
-// function bye() {
-//   console.log('Bye');
+// function evenNumbers(number) {
+//     if(number % 2 === 0){
+//         console.log(number);
+//     }
 // }
-sum(result,10,20);
 
-function sum(callBack,a,b){
-    let r =a + b;
-    callBack(r);
+// function oddNumbers(number) {
+//     if(number % 2 !== 0){
+//         console.log(number);
+//     }
+// }
+
+let fruits = ['apple', 'banana', 'mango', 'orange', 'strawberry'];
+
+
+fruits.forEach(capitilize);
+fruits.forEach(displayFruits);
+
+function upperCase(element,index,array) {
+    array[index] = element.toUpperCase();
 }
 
-function result(r){
-    console.log(r);
+function lowerCase(element,index,array) {
+    array[index] = element.toLowerCase();
+}
+
+function displayFruits(fruit) {
+    console.log(fruit);
+}   
+
+function capitilize(element,index,array) {
+    array[index] = element.charAt(0).toUpperCase() + element.slice(1);
 }
