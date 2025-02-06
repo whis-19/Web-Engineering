c554d6bec10d91b3ad80a98948e13704f729e4a7
Muhammad Abdullah <ma3472995@gmail.com>
2025-02-06 08:08:23 +0500

    Async/Await

--- scripts/index.js
+++ scripts/index.js
@@ -9,57 +9,23 @@
 
 //.reduce(fn): method applies a function against an accumulator and each element in the array
 
+async function doChores(){
 
-function walkDog(){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-
-            const dogWalked = false;
-
-            if(dogWalked){
-                resolve("You walk the dog 🐕");
-            }
-            else{
-                reject("You DIDN'T walk the dog");
-            }
-        }, 1500);
-    });
+    try{
+        const walkDogResult = await walkDog();
+        console.log(walkDogResult);
+    
+        const cleanKitchenResult = await cleanKitchen();
+        console.log(cleanKitchenResult);
+    
+        const takeOutTrashResult = await takeOutTrash();
+        console.log(takeOutTrashResult);
+        
+        console.log("You finsihed all the chores!");
+    }
+    catch(error){
+        console.error(error);
+    }
 }
 
-function cleanKitchen(){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            
-            const kitchenCleaned = true;
-
-            if(kitchenCleaned){
-                resolve("You clean the kitchen 🧹");
-            }
-            else{
-                reject("You DIDN'T clean the kitchen");
-            }
-        }, 2500);
-    });
-}
-
-function takeOutTrash(){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-
-            const trashTakenOut = true;
-
-            if(trashTakenOut){
-                resolve("You take out the trash ♻");
-            }
-            else{
-                reject("You DIDN'T take out the trash");
-            }
-
-        }, 500);
-    });
-}
-
-walkDog().then(value => {console.log(value); return cleanKitchen()})
-                  .then(value => {console.log(value); return takeOutTrash()})
-                  .then(value => {console.log(value); console.log("You finished all the chores!")})
-                  .catch(error => console.error(error));
+doChores();
